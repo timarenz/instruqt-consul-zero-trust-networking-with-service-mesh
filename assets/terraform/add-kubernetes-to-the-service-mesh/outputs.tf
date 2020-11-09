a277492a231143be1b66c4a72be60bb5ee088413
Tim Arenz <tim@arenz.cc>
2020-11-09 17:48:14 +0100

    Fixing outputs

--- assets/terraform/add-kubernetes-to-the-service-mesh/outputs.tf
+++ assets/terraform/add-kubernetes-to-the-service-mesh/outputs.tf
@@ -1,7 +1,7 @@
-outputs "kubeconfig_file" {
+output "kubeconfig_file" {
   value = local_file.kubeconfig.filename
 }
 
-outputs "kubernetes_namespaces_consul" {
+output "kubernetes_namespaces_consul" {
   value = kubernetes_namespace.consul.metadata[0].name
 }
