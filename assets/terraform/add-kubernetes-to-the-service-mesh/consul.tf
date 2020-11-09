d7bab36ee78fb9b89ebf4b6f6afeda880823c341
Tim Arenz <tim@arenz.cc>
2020-11-09 07:06:16 +0100

    Add ingress gateway to test in instruqt

--- assets/terraform/add-kubernetes-to-the-service-mesh/consul.tf
+++ assets/terraform/add-kubernetes-to-the-service-mesh/consul.tf
@@ -198,6 +198,16 @@ resource "helm_release" "consul" {
     value = true
   }
 
+  # set {
+  #   name  = "ingressGateways.enabled"
+  #   value = true
+  # }
+
+  # set {
+  #   name  = "ingressGateways.defaults.service.type"
+  #   value = LoadBalancer
+  # }
+
   values = [<<EOF
   server:
     extraConfig: |
