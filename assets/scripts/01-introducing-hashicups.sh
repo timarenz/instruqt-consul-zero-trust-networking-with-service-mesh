2d3e7303918020202b909e11ddcf8267b84b28d9
Tim Arenz <tim@arenz.cc>
2020-11-12 09:36:32 +0100

    make script executable

--- assets/scripts/01-introducing-hashicups.sh
+++ assets/scripts/01-introducing-hashicups.sh
@@ -6,12 +6,12 @@ echo "Connecting to postgres-server..."
 echo
 echo "Below you see that currently the postgres-services is listening on 0.0.0.0:5432. So anyone in the network can access this services."
 echo
-ssh -i ssh.key consul-admin@postgres-server sudo netstat -tulpn
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ssh.key consul-admin@postgres-server sudo netstat -tulpn
 echo
 echo "Next we dump the traffic of the postgres-services port. If you look closely you will see SQL statements in clear text. To stop the traffic sniffing press Control+C (Mac) or Strg+C (PC)."
-read -n 1 -r -s -p "Press any key to continue..."
+read -p "Press any key to continue..." REPLY
 echo
-ssh -i ssh.key consul-admin@postgres-server sudo tcpdump -nX -i ens4 port 5432 or port 21000
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ssh.key consul-admin@postgres-server sudo tcpdump -nX -i ens4 port 5432 or port 21000
 echo
 echo "All done."
 echo
