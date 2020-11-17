247bccaec2aa8e9324841e9dc2fc801a59fab680
cpu601 <cpu@hashicorp.com>
2020-11-17 11:36:14 +0100

    tail

--- assets/scripts/audit.sh
+++ assets/scripts/audit.sh
@@ -2,5 +2,5 @@
 
 echo "Connecting to consul-server and read audit log..."
 echo
-ssh consul-admin@consul-server sudo cat /var/log/consul/audit-*.json | jq
+ssh consul-admin@consul-server sudo tail -f /var/log/consul/audit-*.json | jq
 echo
